4fd351cccb3680b480b845b3e9a049425204faca
Michel Khalaf <Khalafmichel98@gmail.com>
2021-09-16 10:28:44 +0200

    merge

--- solutions/02.sh
+++ solutions/02.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-<<<<<<< HEAD
-# How many files (excluding directories) are there in ~/?
-#reponse: 5
-
-ls | wc -l
-=======
 # question: how many files in ./
 # answer: 5
 
 ssh ctf ls | wc -l
->>>>>>> 5139200d883aa0e176a54398fb1bf3d38b6c467d
--- solutions/19.sh
+++ solutions/19.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 # How many directories are there in the ~/MLclass/ directory?
-<<<<<<< HEAD
-#reponse: 17
-
-find MLclass/* -maxdepth 0 -type d | wc -l 
-
-=======
 # Reponse: 17
 
 find MLclass/* -maxdepth 0 -type d | wc -l
->>>>>>> 5139200d883aa0e176a54398fb1bf3d38b6c467d
