6bd5870cb434fd502bc1ebb06d3aed41aff3306f
QuCM <q.martincocher@gmail.com>
2021-09-16 13:22:48 +0200

    Q31

---
+++ solutions/31.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# What is the full version of the commit ID of the first commit of the ~/transformers repository?
+# 43badf217d1ccfaf486e2cbb1b3567226b5e95bf
+
+git rev-list --max-parents=0 HEAD
