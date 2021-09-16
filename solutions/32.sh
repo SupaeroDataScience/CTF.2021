25b1831be623c71384fa28c71381300c9237e09f
Stanislas-dura <stanislas.dura@student.isae-supaero.fr>
2021-09-16 10:45:52 +0200

    solution.sh

---
+++ solutions/32.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+#What is the full version of the commit id of the most recent commit of the ~/transformers repo
+#response:#commit 89da1bfeac23ebe722f45f28a46fddc7dcb07bd9 (HEAD -> master, origin/master, origin/HEAD) 
+	#Author: Stas Bekman <stas00@users.noreply.github.com>
+	#Date:   Wed Sep 15 17:18:34 2021 -0700
+git log -1
+
