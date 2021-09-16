ca6c3fb0feea995c9085faa1419d9774c3a502f3
f.delpech <florian.delpech40@orange.fr>
2021-09-16 11:04:19 +0200

    Answer to question 33

--- solutions/33.sh
+++ solutions/33.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Question 33 : What is the most recent commit message of ~/transformers repository?
 # Answer : nightly: add deepspeed master
-cd ~/transformers
-git log -n 1
+ssh ctf cd ~/transformers git log -n 1
